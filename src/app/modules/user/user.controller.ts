35fdef5a4359c0e5bcb7536cacaba85a6d68629c
Sumon <msk.sumon.kaysar@gmail.com>
2025-09-04 14:26:16 +0600

    added user.controller.ts

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
+import httpStatus from "../../utils/httpStatus";
+import sendResponse from "../../utils/sendResponse";
+import { UserServices } from "./user.service";
+
+const updateUser = catchAsync(async (req: Request, res: Response) => {
+  const { id: userId } = req.params;
+  //   const decoded = req.user;
+  const result = await UserServices.updateUser(
+    userId,
+    req.body
+    // decoded as JwtPayload
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User updated succesfully",
+    data: result,
+  });
+});
+
+export const UserControllers = {
+  updateUser,
+};
