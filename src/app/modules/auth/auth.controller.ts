0429dfcecd544400e8cf03b3830b320ed8fee4a7
Sumon <msk.sumon.kaysar@gmail.com>
2025-09-07 14:16:09 +0600

    added logout in auth.controller.ts

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -29,7 +29,29 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const logout = catchAsync(async (req: Request, res: Response) => {
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User logged out successfully",
+    data: null,
+  });
+});
+
 export const AuthControllers = {
   registerUser,
   credentialsLogin,
+  logout,
 };
